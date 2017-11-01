a1e63268540672138cae9b12716470d473a68541
wenwen.xu <wenwen.xu@busonline.com>
2017-11-01 15:10:52 +0800

    提交正则相关

--- app/src/main/java/com/xwtiger/devrescollect/TestJava.java
+++ app/src/main/java/com/xwtiger/devrescollect/TestJava.java
@@ -1,5 +1,6 @@
 package com.xwtiger.devrescollect;
 
+import android.provider.Settings;
 import android.util.Log;
 
 import com.xwtiger.devrescollect.study.javaapi.PatternStudy;
@@ -13,13 +14,20 @@ public class TestJava {
 
 
     public static void main(String[] args){
-
+        testReg1(PatternStudy.REG_GRE);
+        testReg1(PatternStudy.REG_REL);
+        testReg1(PatternStudy.REG_POS);
 
     }
 
-    public static void testReg(){
-        boolean regex = PatternStudy.regex("");
-        System.out.println("regex = "+regex);
+    public static void testReg1(String reg){
+        long startTime = System.currentTimeMillis();
+        PatternStudy.regex3(reg,"123456f005f00");
+        long enttime = System.currentTimeMillis() -startTime;
+        System.out.println("reg = "+reg);
+        //System.out.println("regex = "+regex);
+        System.out.println("enttime = "+enttime);
+        System.out.println("------分割线--------");
     }
 
 }
--- app/src/main/java/com/xwtiger/devrescollect/study/javaapi/PatternStudy.java
+++ app/src/main/java/com/xwtiger/devrescollect/study/javaapi/PatternStudy.java
@@ -14,21 +14,31 @@ import java.util.regex.Pattern;
  */
 public class PatternStudy {
 
-    public static String reg = "";
+    public static String REG_POS = ".*+f00";
+    public static String REG_REL = ".*?f00";
+    public static String REG_GRE = ".*f00";
     /**
      * 官方表示
      *  Pattern p = Pattern.compile("a*b");
         Matcher m = p.matcher("aaaaab");
         boolean b = m.matches();
      */
-    public static boolean regex(String input){
+    public static boolean regex(String reg,String input){
         Pattern p = Pattern.compile(reg);
         Matcher matcher = p.matcher(input);
         return matcher.matches();
     }
 
-    public static boolean regex2(String input){
+    public static boolean regex2(String reg,String input){
         return Pattern.matches(reg,input);
+    }
+
+    public static void regex3(String reg,String input){
+        Pattern p = Pattern.compile(reg);
+        Matcher matcher = p.matcher(input);
+        while (matcher.find()) {
+            System.out.println("matched form " + matcher.start() + " to " + matcher.end());
+        }
     }
 
     /**
@@ -37,9 +47,20 @@ public class PatternStudy {
      *  贪婪的  勉强的    所有的
      *
      * 这三者的区别
+     * greedy量词被看作“贪婪的”，因为它们在试图搜索第一个匹配之前读完（或者说吃掉）整个输入字符串。
+     * 如果第一个匹配尝试（整个输入字符串）失败，匹配器就会在输入字符串中后退一个字符并且再次尝试，
+     * 重复这个过程，直到找到匹配或者没有更多剩下的字符可以后退为止。根据表达式中使用的量词，
+     * 它最后试图匹配的内容是1个或者0个字符。
      *
+     但是，reluctant量词采取相反的方式：它们从输入字符串的开头开始，
+     然后逐步地一次读取一个字符搜索匹配。
+     它们最后试图匹配的内容是整个输入字符串。
+
+     最后，possessive量词总是读完整个输入字符串，
+     尝试一次（而且只有一次）匹配。和greedy量词不同，
+     possessive从不后退，即使这样做能允许整体匹配成功
      *
-     *
+     * Possessive 一般针对精确的正则；eg:".{5}+f00"(精确了具体的数字)
      *
      */
 
