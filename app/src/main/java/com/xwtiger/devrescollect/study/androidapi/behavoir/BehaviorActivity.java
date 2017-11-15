e7c48a4420eaec1982d41c18709fd06100c0dd3f
wenwen.xu <wenwen.xu@busonline.com>
2017-11-15 19:52:10 +0800

    添加behavoir

---
+++ app/src/main/java/com/xwtiger/devrescollect/study/androidapi/behavoir/BehaviorActivity.java
@@ -0,0 +1,21 @@
+package com.xwtiger.devrescollect.study.androidapi.behavoir;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.xwtiger.devrescollect.R;
+
+/**
+ * Created by Busap-112 on 2017/11/15.
+ */
+
+public class BehaviorActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testbehavoir);
+
+    }
+}
