45014d4a1425852a40a1728738a3279164169348
wenwen.xu <wenwen.xu@busonline.com>
2018-01-02 21:14:24 +0800

    add design pattern

--- app/src/main/java/com/xwtiger/devrescollect/study/algorithm/TestAlgorithm.java
+++ app/src/main/java/com/xwtiger/devrescollect/study/algorithm/TestAlgorithm.java
@@ -9,7 +9,7 @@ public class TestAlgorithm {
     public static void main(String[] args){
         /*int nDisks = 4;
         doTowers(nDisks, 'A', 'B', 'C');*/
-        int x = 2;
+        System.out.println("haha");
         System.out.println(sum(4));
         int y = 3;
     }
