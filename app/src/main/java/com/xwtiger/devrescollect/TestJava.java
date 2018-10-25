cfcbaee101ac0b34dbee7e3a12060a134db21d5e
胥文 <xuww@youshu.cc>
2018-10-25 10:46:08 +0800

    test branch2 3

--- app/src/main/java/com/xwtiger/devrescollect/TestJava.java
+++ app/src/main/java/com/xwtiger/devrescollect/TestJava.java
@@ -73,6 +73,7 @@ public class TestJava extends TestUapte{
        //data:image/png;base64
        System.out.println("test1");
        System.out.println("test2");
+       System.out.println("test3");
 
 
     }
