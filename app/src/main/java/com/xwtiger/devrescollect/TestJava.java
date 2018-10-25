1e3c8c9e74f48451765ef76fe308d0cfe8ff9878
胥文 <xuww@youshu.cc>
2018-10-25 11:38:54 +0800

    test

--- app/src/main/java/com/xwtiger/devrescollect/TestJava.java
+++ app/src/main/java/com/xwtiger/devrescollect/TestJava.java
@@ -78,6 +78,7 @@ public class TestJava extends TestUapte{
        System.out.println(" master test5");
        System.out.println("testbrand2 test4");
        System.out.println("testbrand3 test7");
+       System.out.println("master test9");
 
 
     }
