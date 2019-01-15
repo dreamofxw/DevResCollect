5207ad4934581476c824642314a25c0f1d0fa597
胥文 <xuww@youshu.cc>
2019-01-15 09:52:22 +0800

    test mac as commit

--- app/src/main/java/com/xwtiger/devrescollect/TestJava.java
+++ app/src/main/java/com/xwtiger/devrescollect/TestJava.java
@@ -141,6 +141,7 @@ public class TestJava extends TestUapte{
             System.out.println("test mac submit");
             System.out.println("test mac as submit");
             System.out.println("test mac as submit add shell ");
+            System.out.println("test mac as submit audo add shell");
 
 
 
