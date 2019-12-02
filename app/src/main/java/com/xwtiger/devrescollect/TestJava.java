05a1e8d25d9cc6df4fa9ac2a7687317b9dc632cd
胥文 <xuww@youshu.cc>
2019-12-02 17:47:31 +0800

    commit testgit3 first

--- app/src/main/java/com/xwtiger/devrescollect/TestJava.java
+++ app/src/main/java/com/xwtiger/devrescollect/TestJava.java
@@ -436,7 +436,7 @@ public class TestJava extends TestUapte{
         System.out.println("s="+s);
         
         System.out.println("test git2");
-
+        System.out.println("test git3");
 
         System.out.println("add git branch testgit1");
     }
