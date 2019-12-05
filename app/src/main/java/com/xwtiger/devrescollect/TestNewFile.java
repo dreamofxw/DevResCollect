19d52f17168bead0a46398399cdb75aab8afabf8
胥文 <xuww@youshu.cc>
2019-12-05 11:43:06 +0800

    test add and modify

---
+++ app/src/main/java/com/xwtiger/devrescollect/TestNewFile.java
@@ -0,0 +1,9 @@
+package com.xwtiger.devrescollect;
+
+import android.content.SyncStatusObserver;
+
+public class TestNewFile {
+    public static void main(String[] args){
+        System.out.println("print main");
+    }
+}
