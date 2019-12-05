bd076873d48165a1815e5c6999928f2f2f0a6161
胥文 <xuww@youshu.cc>
2019-12-05 14:27:46 +0800

    modify commit first

--- app/src/main/java/com/xwtiger/devrescollect/TestJava.java
+++ app/src/main/java/com/xwtiger/devrescollect/TestJava.java
@@ -443,6 +443,7 @@ public class TestJava extends TestUapte{
         System.out.println("test gitcommit222");
         System.out.println("test gitcommit333");
         System.out.println("test gitcommit444");
+        System.out.println("test gitcommit555");
 
         System.out.println("add git branch testgit1");
     }
