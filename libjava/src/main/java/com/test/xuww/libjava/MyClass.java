a4425d94a9f8da71154f26ea013aa7a6187d3e52
胥文 <xuww@youshu.cc>
2020-04-26 11:00:42 +0800

    添加文档

--- libjava/src/main/java/com/test/xuww/libjava/MyClass.java
+++ libjava/src/main/java/com/test/xuww/libjava/MyClass.java
@@ -6,26 +6,21 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 public class MyClass {
 
+
+
+
     public static void main(String[] args) {
-
-
         String str = "5.09.0";
         System.out.println(str.replaceAll("\\.",""));
-
-
-
     }
 
 
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
     public static LinkedBlockingQueue<String> linkedBlockingQueue = new LinkedBlockingQueue<String>();
-
-
-
-
-
-
-
-
     public static String intToStringForJoinNums(int join_nums){
         if(join_nums >=10000){
             float newvlaue = join_nums / 10000f;
