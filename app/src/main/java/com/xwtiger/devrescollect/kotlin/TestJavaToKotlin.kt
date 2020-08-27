5f4ab48c79eba37efcded140a1bbd17234da3584
胥文 <0928xuwwen@sina.com>
2020-08-27 19:20:43 +0800

    test

---
+++ app/src/main/java/com/xwtiger/devrescollect/kotlin/TestJavaToKotlin.kt
@@ -0,0 +1,12 @@
+package com.xwtiger.devrescollect.kotlin
+
+/**
+ * @author : 胥文
+ * @date : 2020/8/22 9:55
+ * @desc :
+ */
+class TestJavaToKotlin {
+    var name: String? = null
+    var age = 0
+    var sex = false
+}
---
+++ app/src/main/java/com/xwtiger/devrescollect/kotlin/model/TestPerson.kt
@@ -0,0 +1,10 @@
+package com.xwtiger.devrescollect.kotlin.model
+
+/**
+ * @author : 胥文
+ * @date   : 2020/8/22 9:49
+ * @desc   :
+ */
+
+object TestPerson {
+}
